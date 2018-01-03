2507b9c3e231114bb76f834e67ad0505d7f94c0d
balaji <damodaran.balaji@gmail.com>
2018-01-03 19:39:18 +0530

    a better version of phone number

--- erlang/phone-number/src/phone_number.erl
+++ erlang/phone-number/src/phone_number.erl
@@ -2,21 +2,16 @@
 
 -export([number/1, areacode/1, pretty_print/1, test_version/0]).
 
-number(String) -> check(list_to_integer(number(String, ""))).
+number(Number) -> number_internal([C || C <- Number, C >= $0, C =< $9]).
 
-number([], Acc) -> Acc;
-number([H | T], Acc) when H >= $0 andalso H =< $9 -> number(T, Acc ++ [H]);
-number([_ | T], Acc) -> number(T, Acc).
+number_internal([$1|Number]) when length(Number) =:= 10 -> Number;
+number_internal(Number) when length(Number) =:= 10 -> Number;
+number_internal(_) -> string:copies("0", 10).
 
-check(Number) when Number div 10000000000 =:= 1 -> integer_to_list(Number - 10000000000);
-check(Number) when Number div 10000000000 > 0 -> "0000000000";
-check(Number) when Number div 1000000000 /= 0 -> integer_to_list(Number);
-check(_) -> "0000000000".
+areacode(Number) -> string:substr(number(Number), 1, 3).
 
-areacode(String) -> string:substr(number(String), 1, 3).
-
-pretty_print(String) ->
-  Number = number(String),
+pretty_print(Num) ->
+  Number = number(Num),
   lists:flatten(io_lib:format("(~s) ~s-~s",
     [lists:sublist(Number, 1, 3),
       lists:sublist(Number, 4, 3),
