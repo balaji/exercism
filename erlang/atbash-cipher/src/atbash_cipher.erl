4c31b00b5ed153f201f5375b714c134f9cae156d
balaji <damodaran.balaji@gmail.com>
2018-01-03 17:40:21 +0530

    atbash-cipher. erlang

---
+++ erlang/atbash-cipher/src/atbash_cipher.erl
@@ -0,0 +1,25 @@
+-module(atbash_cipher).
+
+-export([encode/1, decode/1, test_version/0]).
+
+encode(String) -> insert_spaces(plain_encode(string:to_lower(String)), "", "").
+
+plain_encode(String) -> plain_encode(String, "").
+plain_encode([], Encoded) -> Encoded;
+plain_encode([H | T], Encoded) when H >= $a andalso H =< $z -> plain_encode(T, Encoded ++ [($a + $z) - H]);
+plain_encode([H | T], Encoded) when H >= $1 andalso H =< $9 -> plain_encode(T, Encoded ++ [H]);
+plain_encode([_ | T], Encoded) -> plain_encode(T, Encoded).
+
+insert_spaces([], Temp, Acc) -> Acc ++ Temp;
+insert_spaces([H | T], Temp, Acc) ->
+  case string:length(Temp) == 5 of
+    false -> insert_spaces(T, Temp ++ [H], Acc);
+    _ -> insert_spaces(T, [H], Acc ++ Temp ++ [$ ])
+  end.
+
+decode(String) -> decode(String, "").
+
+decode([], Acc) -> Acc;
+decode([H | T], Acc) -> plain_encode(T, Acc ++ plain_encode([H])).
+
+test_version() -> 1.
