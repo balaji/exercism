6c5c06784f49261df3976566e7fab35b768d803e
balaji <damodaran.balaji@gmail.com>
2018-01-03 13:22:33 +0530

    erlang triangle

--- erlang/triangle/src/triangle.erl
+++ erlang/triangle/src/triangle.erl
@@ -2,7 +2,10 @@
 
 -export([kind/3, test_version/0]).
 
-kind(A, B, C) ->
-  undefined.
+kind(A, B, C) when A =< 0 orelse B =< 0 orelse C =< 0 -> {error, "all side lengths must be positive"};
+kind(A, B, C) when A + B =< C orelse A + C =< B orelse B + C =< A -> {error, "side lengths violate triangle inequality"};
+kind(A, A, A) -> equilateral;
+kind(A, B, C) when A =:= B orelse A =:= C orelse B =:= C -> isosceles;
+kind(_, _, _) -> scalene.
 
 test_version() -> 1.
